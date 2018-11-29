87372b81fa098ba80d85153ff66f21f4703ee078
Rondou Chen <40and44sis@gmail.com>
2018-11-29 19:04:56 +0800

    Init pytest mock

--- test/test_monitor.py
+++ test/test_monitor.py
@@ -2,19 +2,32 @@
 
 from typing import Any, Callable, Dict, List, Optional, Union, NewType, Iterable, TypeVar
 from sysinfo import monitor
-from unittest.mock import MagicMock
+import json
 
 
+def test_info_meta_path(mocker):
+    meta = monitor.Meta2(path='/test/1')
 
-def test_info():
-    meta = monitor.Meta2(path='/test/123')
-
-    monitor.load_json_data_from_json_file = MagicMock()
-    monitor.load_json_data_from_json_file.return_value = 'test'
-
-    monitor.monitor_generator = MagicMock()
-    monitor.monitor_generator.return_value = None
+    load_json_data_from_json_file = mocker.patch('sysinfo.monitor.load_json_data_from_json_file', return_value=None)
+    mocker.patch('sysinfo.monitor.monitor_generator', return_value=None)
 
     monitor.info(meta)
 
-    monitor.load_json_data_from_json_file.assert_called_with(meta.path)
+    load_json_data_from_json_file.assert_called_with(meta.path)
+
+
+def test_info_dump_json(mocker):
+    meta = monitor.Meta2(path='/test/2')
+
+    mocker.patch('sysinfo.monitor.load_json_data_from_json_file', return_value=None)
+    mocker.patch('sysinfo.monitor.monitor_generator', return_value={"key1": 123, "key2": "456", "key3": True})
+
+    assert json.loads(monitor.info(meta)) == json.loads('{"key3": true, "key1": 123, "key2": "456"}')
+
+
+def test_info_raises(mocker):
+    meta = monitor.Meta2(path='/test/3')
+    mocker.patch('sysinfo.monitor.load_json_data_from_json_file',
+                 side_effect=json.JSONDecodeError(msg='test', doc='{"keys"}', pos=7))
+
+    assert monitor.info(meta) == 'JSONDecodeError'
